a3249a5cc228c13a9504b07204228cb95df6f1ba
xhrl8649 <xhrl8649@gmail.com>
2022-11-20 16:58:06 +0900

    색상 정보 db에 저장

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,11 +5,18 @@ const spaceSize = 16.0;
 const primaryColor = Colors.purple;
 
 const categoryColors = [
+  //빨강
   'F44336',
+  //주황
   'FF9800',
+  //노랑
   'FFEB3B',
+  //초록
   '4CAF50',
+  //하늘
   '2196F3',
+  //파랑
   '3F51B5',
+  //보라
   '9C27B0',
 ];
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
 import 'package:calendar_scheduler_study/database/drift_database.dart';
 import 'package:calendar_scheduler_study/screens/home.screen.dart';
 import 'package:calendar_scheduler_study/theme.dart';
+import 'package:drift/drift.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:calendar_scheduler_study/constants.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // ! 01 main을 async로 변경할 때 필수
   await initializeDateFormatting(); // ! 캘린더 다국어 지원
 
   final database = LocalDatabase();
+
+  final colors = await database.getCategoryColors();
+
+  if(colors.isEmpty){
+    for(String hexCode in categoryColors){
+      await database.createCategoryColor(
+        CategoryColorsCompanion(
+          hexCode: Value(hexCode)
+        ),
+      );
+    }
+  }
 
   runApp(const MyApp());
 }
